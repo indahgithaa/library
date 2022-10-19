9509cfc698cc74c8301da0aa7f097fe62cd173ff
indahgitha <indahgitha99@gmail.com>
2022-10-20 01:07:46 +0800

    Update basic UI

---
+++ script.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector('.modal')
+const addButton = document.querySelector('#addButton')
+
+addButton.addEventListener('click', () => {
+    modal.style.display = "block"
+})

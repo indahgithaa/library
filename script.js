c07249b60acf6a0494c21bbba9f63118964f6099
indahgitha <indahgitha99@gmail.com>
2022-10-21 23:55:46 +0800

    handling object

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 /* WHAT TO DO:
 1. receive value from each form inside the modals.
 2. store the form values inside the constructors using new keyword 
-3. create card and the book's elements, give data attributes for each.
-4. append each card to container through loops.
+3. push the book data to the array.
+4. create card and the book's elements, give data attributes for each.
+5. append each card to container through loops.
 features:
 - sort (date, read, not read, favorites)
 - reset books/container */
 
-
 /* Book Elements */
 const container = document.querySelector('.book-container')
-const bookTitle = document.querySelector('#book-title')
-const bookAuthor = document.querySelector('#author')
-const bookPages = document.querySelector('#pages')
-const bookStatus = document.querySelector('#status-name')
+const bTitle = document.querySelector('#book-title')
+const bAuthor = document.querySelector('#author')
+const bPages = document.querySelector('#pages')
+const bStatus = document.querySelector('#status-name')
 
 /* Modal Elements */
 const modal = document.querySelector('.modal')
@@ -23,20 +23,35 @@ const submit = document.querySelector('#submit-button')
 const reset = document.querySelector('#reset-button')
 
 /* ----- Library Functionalities start here ----- */
-let myLibrary = []
+let myLibrary = [];
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
+    this.readStatus = readStatus;
+};
+
+Book.prototype.titleInfo = function() {
+    this.title = bTitle.value;
+    return this.title;
+};
+
+Book.prototype.authorInfo = function() {
+    this.author = bAuthor.value;
+    return this.author;
+};
+
+Book.prototype.pagesInfo = function() {
+    this.pages = bPages.value;
+    return this.pages;
 }
 
-Book.prototype.read = function() {
-    if (this.status === "read") {
-        this.status = "read"
-    } else if (this.status === "unread") {
-        this.status = "not read"
+Book.prototype.readInfo = function() {
+    if (bStatus.checked) {
+        this.readStatus = "read"
+    } else {
+        this.readStatus = "not read"
     }
 }
 
@@ -46,16 +61,25 @@ addBtn.addEventListener('click', () => {
 })
 
 //close modal
-
 closeBtn.onclick = () => {
     modal.style.display = "none"
 }
 
-/* Allow users to close the modal from everywhere: https://stackoverflow.com/questions/37573608/how-to-make-modal-close-on-click-outside */
+// Allow users to close the modal from everywhere: https://stackoverflow.com/questions/37573608/how-to-make-modal-close-on-click-outside 
 window.onclick = function(e) {
     if (e.target == modal) {
         modal.style.display = "none"
     }
 }
+
+function addBookToLibrary() {
+    const book = new Book(bTitle.value, bAuthor.value, bPages.value, bStatus.value)
+    myLibrary.push(book)
+}
+
+submit.addEventListener('click', () => {
+    addBookToLibrary()
+    console.log(myLibrary)
+})
 
 
